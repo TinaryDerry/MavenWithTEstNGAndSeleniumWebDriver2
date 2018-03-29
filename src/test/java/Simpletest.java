2a6b497cff759ced709e4a4afe4f28bee431e85c
TinaryDerry <37700444+TinaryDerry@users.noreply.github.com>
2018-03-29 11:52:45 +0300

    Update Simpletest.java

--- src/test/java/Simpletest.java
+++ src/test/java/Simpletest.java
@@ -8,12 +8,16 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.Assert;
 import org.openqa.selenium.interactions.Actions;
+
 import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 import java.io.*;
 
 public class Simpletest {
+
     private WebDriver driver;
     private WebDriverWait wait;
 
@@ -26,55 +30,62 @@ public class Simpletest {
 
     @Test
     public void mySamTest() throws Exception {
+        driver.get("https://www.google.ru/");
+        WebElement Googla = driver.findElement(By.id("lst-ib"));
+        Googla.click();
+        Googla.sendKeys("Вконтакте" + Keys.ENTER);
+        Thread.sleep(40);
+        WebElement VK = driver.findElement(By.linkText("ВКонтакте: Добро пожаловать"));
+        VK.click();
+        ArrayList tabs2 = new ArrayList (driver.getWindowHandles());//Получение списка табов
+        driver.switchTo().window(tabs2.get(1));//Переключение на второй таб в браузере
 
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        // WebElement element = driver.findElements(By.cssSelector("")).get(1);
-        //element.click();
-        driver.get("https://vk.com/stolica_chernozemia"); //страница вк
-        WebElement myWebEl = driver.findElements(By.cssSelector("[class='wall_post_text']")).get(0); // гет () - значения которого по счету поста необходимо вернуть
-        String Zap = myWebEl.getAttribute("textContent");
-        try (FileWriter writer = new FileWriter("F:\\Itogo\\test1.txt", true)) {
-            String text = Zap;
-            writer.write(text);
-            // запись по символам
-            writer.append('\n');
-            writer.append('\\');
-            writer.flush();
-        }
-        //поиск по СЛОВУ в записях на стене
-        driver.findElement(By.cssSelector("[class='ui_tab_plain ui_tab_search']")).click();
-        WebElement Teg2 = driver.findElement(By.cssSelector("[id='wall_search']"));
-        Teg2.click();
-        Teg2.sendKeys("Фотографии"+ Keys.ENTER);//слово поиска
-
-        WebElement Z1 = driver.findElements(By.cssSelector("[class= 'ui_search_field _field']")).get(0);
-        String Tag1 = Z1.getAttribute("textContent");
-
-        try (FileWriter writer = new FileWriter("F:\\Itogo\\test2.txt", true)) {
-            String text = Tag1;
-            writer.write(text);
-            writer.append('\n');
-            writer.append('\\');
-            writer.flush();
-        }
-
-        catch (IOException ex) {
-
-                System.out.println(ex.getMessage());
-            }
-
-        }
+//ДРУГОЙ ТЕСТ ДЛЯ ВК.
+//        Thread.sleep(100);
 
 
-        // Actions actions = new Actions(driver).click(); //наведение мыши на элемент
-        // actions.moveToElement(element).build().perform();// наведение мыши на элемент
-
-
+//        Login.sendKeys("eqweqweqweqwe"+ Keys.ENTER);
+//
+////[id='index_pass']"
+//        WebElement Password = driver.findElement(By.cssSelector("index_pass"));
+//        Password.sendKeys("224416T");
+//        // Assert.assertTrue(menuName.equals("Медиа"));
+//
+//        // driver.findElement(By.xpath(".//*[@id='rso']")).click();
+//
+//
+//        //WebElement myWebEl = driver.findElements(By.cssSelector("[class='wall_post_text']")).get(0);
+//        // String Zap = myWebEl.getAttribute("textContent");  //обращаемся к свойству
+//        //try (FileWriter writer = new FileWriter("F:\\Itogo\\test1.txt", true)) {
+//        //  String text = Zap;
+//        //   writer.write(text);
+//        // запись по символам
+//        //   writer.append('\n');
+//        //  writer.append('\\');
+//        // writer.flush();
     }
+    //поиск по СЛОВУ в записях на стене
+    // driver.findElement(By.cssSelector("[class='ui_tab_plain ui_tab_search']")).click();
+    //WebElement Teg2 = driver.findElement(By.cssSelector("[id='wall_search']"));
+    //Teg2.click();
+    //Teg2.sendKeys("Фотографии"+ Keys.ENTER);//слово поиска
+    // WebElement Z1 = driver.findElements(By.cssSelector("[class= 'ui_search_field _field']")).get(0);
+    //String Tag1 = Z1.getAttribute("textContent");
 
+    // try (FileWriter writer = new FileWriter("F:\\Itogo\\test2.txt", true)) {
+    // String text = Tag1;
+    // writer.write(text);
+    // writer.append('\n');
+    // writer.append('\\');
+    // writer.flush();
+    // }
 
+    //  catch (IOException ex) {
 
-
-
-
+    //      System.out.println(ex.getMessage());
+    // }
+//
+    //  }
+//
+}
 
